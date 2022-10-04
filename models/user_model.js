c9a0d307830906d8d762d6943bd975473b0d60fb
Hoon <yimsh4507@gmail.com>
2022-10-03 23:42:21 -0400

    a schema for user has been implemented

--- models/user_model.js
+++ models/user_model.js
@@ -1,1 +1,31 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
+
+const user_schema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: [true, "Please provide username.."]
+    },
+    email: {
+        type: String,
+        required: [true, "Please provide email.."],
+        unique: true,
+        lowercase: true,
+        validate: [validator.isEmail, "Provided email is not in valid form.."]
+    },
+    password: {
+        type: String,
+        required: [true, "Please provide password.."],
+        minlength: 4
+    },
+    passwordConfirm: {
+        type: String,
+        required: [true, "Please re-enter your password.."],
+        validate: {
+            validator: function (password) {
+                return this.password === password;
+            },
+            message: "re-entered password is not identical.."
+        }
+    }
+});

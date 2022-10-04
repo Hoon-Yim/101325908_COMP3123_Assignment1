f186f3e854904b52bb0abc471c16677076496323
Hoon <yimsh4507@gmail.com>
2022-10-04 12:31:07 -0400

    sample data inserted

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,7 +1,17 @@
-exports.signup = (req, res) => {
+const User = require("../models/user_model");
+
+exports.signup = async (req, res) => {
+    const new_user = await User.create({
+        username: "Hoon",
+        email: "yimsh4507@gmail.com",
+        password: "something",
+        passwordConfirm: "something"
+    });
+
     res.status(201).json({
         status: true,
-        message: "signup() not implemented yet"
+        message: "signup() not implemented yet",
+        data: new_user
     });
 }
 

0d7911e349b2275ad63491aca55f5cc394506460
Hoon <yimsh4507@gmail.com>
2022-10-03 22:36:52 -0400

    user router is being used as a middleware

--- app.js
+++ app.js
@@ -4,4 +4,8 @@ const app = express();
 // body parser
 app.use(express.json());
 
+// routers
+const user_router = require("./routers/user_router");
+app.use("/api/user", user_router);
+
 module.exports = app;

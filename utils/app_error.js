0e6a77f2c1503fed17978d3d0cec01c881f89b30
Hoon <yimsh4507@gmail.com>
2022-10-04 17:25:32 -0400

    custom error handler is created

---
+++ utils/app_error.js
@@ -0,0 +1,12 @@
+class AppError extends Error {
+    constructor(message, status_code) {
+        super(message);
+
+        this.status_code = status_code;
+        this.status = `${status_code}`.startsWith('4') ? false : true;
+
+        Error.captureStackTrace(this, this.constructor);
+    }
+}
+
+module.exports = AppError;

6237638bc08e279b5894f709363feedb50cce726
Hoon <yimsh4507@gmail.com>
2022-10-04 11:41:37 -0400

    DB connection succeeded

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
+// for DB connection
+const mongoose = require("mongoose");
 // for using environmental variables in .env file
 const dotenv = require("dotenv");
 
 const app = require(`${__dirname}/app`);
 
 dotenv.config({ path: `${__dirname}/config.env` });
+
+// DB connection
+const DB = process.env.DB.replace("<password>", process.env.DB_PASSWORD);
+mongoose.connect(DB, {
+    useNewUrlParser: true
+}).then(() => console.log("DB has been successfully connected.."));
 
 // opening server and listening any events
 const PORT = process.env.PORT || 8000;

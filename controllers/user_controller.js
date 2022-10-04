eb2dae28a6967bf225e78c61a03e1a689bcde483
Hoon <yimsh4507@gmail.com>
2022-10-03 22:33:26 -0400

    simple user controller template implemented

---
+++ controllers/user_controller.js
@@ -0,0 +1,13 @@
+exports.signup = (req, res) => {
+    res.status(201).json({
+        status: true,
+        message: "signup() not implemented yet"
+    });
+}
+
+exports.login = (req, res) => {
+    res.status(201).json({
+        status: true,
+        message: "login() not implemented yet"
+    });
+}

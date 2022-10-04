9ef04a5ddc56a29fe81f3c4fd9a290d07d565a1e
Hoon <yimsh4507@gmail.com>
2022-10-04 19:17:42 -0400

    signup() fixed

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,23 +1,30 @@
 const User = require("../models/user_model");
+const catch_async = require("../utils/catch_async");
 
-exports.signup = async (req, res) => {
+exports.signup = catch_async(async (req, res) => {
     const new_user = await User.create({
-        username: "Hoon",
-        email: "yimsh4507@gmail.com",
-        password: "something",
-        passwordConfirm: "something"
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password,
+        password_confirm: req.body.password_confirm
     });
 
     res.status(201).json({
         status: true,
-        message: "signup() not implemented yet",
         data: new_user
     });
-}
+});
 
-exports.login = (req, res) => {
+exports.login = catch_async(async (req, res) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        return next(new AppError("Please provide email and password.."), 400);
+    }
+
+    const user = await User.findOne({ email });
+
     res.status(200).json({
-        status: true,
-        message: "login() not implemented yet"
+        status: true
     });
-}
+});

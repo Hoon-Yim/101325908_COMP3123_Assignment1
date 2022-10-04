73d294259f9ec185dd8098c660a1286db53e4006
Hoon <yimsh4507@gmail.com>
2022-10-04 19:41:01 -0400

    creating JWT and setting cookie has been implemented

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,6 +1,32 @@
+const JWT = require("jsonwebtoken");
+
 const User = require("../models/user_model");
 const AppError = require("../utils/app_error");
 const catch_async = require("../utils/catch_async");
+
+// singing token using _id
+const sign_token = id => {
+    return JWT.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRES_IN
+    });
+}
+
+const create_and_send_token = (user, status_code, res) => {
+    const token = sign_token(user._id);
+    const cookie_options = {
+        expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
+        httpOnly: true
+    }
+
+    // setting cookie to check if the user is logged in or not
+    res.cookie("jwt", token, cookie_options);
+
+    res.status(status_code).json({
+        status: true,
+        token,
+        user
+    });
+}
 
 exports.signup = catch_async(async (req, res) => {
     const new_user = await User.create({
@@ -10,10 +36,7 @@ exports.signup = catch_async(async (req, res) => {
         password_confirm: req.body.password_confirm
     });
 
-    res.status(201).json({
-        status: true,
-        new_user
-    });
+    create_and_send_token(new_user, 201, res);
 });
 
 exports.login = catch_async(async (req, res, next) => {
@@ -29,8 +52,5 @@ exports.login = catch_async(async (req, res, next) => {
         return next(new AppError("Incorrect Email or Password", 401));
     }
 
-    res.status(200).json({
-        status: true,
-        user
-    });
+    create_and_send_token(user, 200, res);
 });

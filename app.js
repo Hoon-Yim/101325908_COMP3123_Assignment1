568d163b0ce5d0e8dc8b3ddcaf31e526b43ebaa4
Hoon <yimsh4507@gmail.com>
2022-10-04 17:38:21 -0400

    global handler using error_controller is added

--- app.js
+++ app.js
@@ -8,4 +8,8 @@ app.use(express.json());
 const user_router = require("./routers/user_router");
 app.use("/api/user", user_router);
 
+// global error handling controller
+const error_controller = require("./controllers/error_controller");
+app.use(error_controller);
+
 module.exports = app;

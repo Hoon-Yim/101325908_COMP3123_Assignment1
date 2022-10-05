b14507776edfbc1c73f8514932bd0471c9d8bba2
Hoon <yimsh4507@gmail.com>
2022-10-04 20:51:00 -0400

    minlength is changed to maxlength

--- models/employee_model.js
+++ models/employee_model.js
@@ -4,24 +4,24 @@ const validator = require("validator");
 const employee_schema = new mongoose.Schema({
     first_name: {
         type: String,
-        minlength: 100,
+        maxlength: 100,
         required: [true, "An employee must have a first name.."]
     },
     last_name: {
         type: String,
-        minlength: 50,
+        maxlength: 50,
         required: [true, "An employee must have a last name.."]
     },
     email: {
         type: String,
         unique: true,
         lowercase: true,
-        minlength: 50,
+        maxlength: 50,
         validate: [validator.isEmail, "Provided email is no in valid format.."]
     },
     gender: {
         type: String,
-        minlength: 25,
+        maxlength: 25,
         enum: ["male", "female", "other"],
         default: "other"
     },

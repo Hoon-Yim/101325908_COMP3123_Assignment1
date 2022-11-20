e1b0f7eadcfafca138af5ffd1ad3d3c902e3a45e
Hoon <yimsh4507@gmail.com>
2022-11-19 22:57:40 -0500

    Updated error_controller.js - handler for jwt malformed error added

--- controllers/error_controller.js
+++ controllers/error_controller.js
@@ -1,3 +1,5 @@
+const AppError = require("../utils/app_error");
+
 const SendError = (err, req, res, next) => {
     return res.status(err.status_code).json({
         status: err.status,
@@ -7,12 +9,16 @@ const SendError = (err, req, res, next) => {
     });
 }
 
+const handleJWTError = () => new AppError("Invalid token.", 401);
+
 module.exports = (err, req, res, next) => {
     err.status_code = err.status_code || 500;
     err.status = err.status == undefined ? "error" : err.status;
 
     let error = { ...err };
     error.message = err.message;
+
+    if (error.name === "JsonWebTokenError") error = handleJWTError();
 
     SendError(error, req, res, next);
 }
